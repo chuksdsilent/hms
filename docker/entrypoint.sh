56b8b7f56bf76277de253d2e43ae04f2578eda7b
Samson <chuksdsilent@gmail.com>
2024-09-06 14:59:35 +0100

    Initial commit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-progress --no-interaction
+    composer update --no-progress --no-interaction
 fi
 
 if [ ! -f ".env" ]; then
